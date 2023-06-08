233ea2c6e2c1d774f647220ec13a9070a4bbcec6
SJG <shinjg97@gmail.com>
2023-06-08 18:01:19 +0900

    project initialize

---
+++ app/src/main/java/com/example/plant/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.plant
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        }
+}
