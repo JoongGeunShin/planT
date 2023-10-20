dcd448eb5205f5377aca2f34e2903e7cc08237d9
SJG <shinjg97@gmail.com>
2023-10-20 18:57:20 +0900

    2023-10-20 메모 순서 QUERY로 정렬

--- app/src/main/java/com/example/plant/main_fragment/calendar/db/ScheduleDao.kt
+++ app/src/main/java/com/example/plant/main_fragment/calendar/db/ScheduleDao.kt
@@ -15,8 +15,10 @@ interface ScheduleDao { // 일정 테이블 관련
     @Query("DELETE FROM schedule WHERE serialNum = :serialNum") // 일련번호로 삭제
     fun deleteSchedule(serialNum : Int)
 
-    // '날짜' 에 해당하는 모든 데이터 가져오기
-    @Query("SELECT * FROM schedule WHERE date = :date")
+    // '날짜' 에 해당하는 모든 데이터 가져오기 -----이쪽 수정해서 해보자-------
+    @Query("""SELECT * FROM schedule WHERE date = :date ORDER BY 
+        start_hour ASC, end_hour ASC
+    """)
     fun getAllSchedule(date: String) : LiveData<List<Schedule>>
 
     // '일련 번호' 에 해당하는 모든 데이터 가져오기
